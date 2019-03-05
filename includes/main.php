784a07eb02f8bf6da7acf597bbb40a09f52933d6
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2019-03-05 12:13:34 +0100

    Modifs Responsive

--- includes/main.php
+++ includes/main.php
@@ -1,10 +1,10 @@
 <div class="row no-gutters">
-    <div class="col-6">
+    <div class="col-12 col-sm-6">
         <div class="action" onclick="location.href='action.php';">
             <h2>Action</h2>
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-12 col-sm-6">
         <div class="verite" onclick="location.href='verite.php';">
             <h2>Vérité</h2>
         </div>
