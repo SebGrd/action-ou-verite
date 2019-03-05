b58d310d8d03c89ce05e09350f12a67be903e52a
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2019-03-05 10:24:59 +0100

    Réglage problème URL
    Liens non fonctionnels sous apache donc ajout de l'extension .php

--- includes/main.php
+++ includes/main.php
@@ -1,11 +1,11 @@
 <div class="row no-gutters">
     <div class="col-6">
-        <div class="action" onclick="location.href='action';">
+        <div class="action" onclick="location.href='action.php';">
             <h2>Action</h2>
         </div>
     </div>
     <div class="col-6">
-        <div class="verite" onclick="location.href='verite';">
+        <div class="verite" onclick="location.href='verite.php';">
             <h2>Vérité</h2>
         </div>
     </div>
